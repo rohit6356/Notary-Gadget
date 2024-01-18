15ce365d30b27d76517958cb847249c6c3d28d43
Rohit Sharma <87434241+rohit6356@users.noreply.github.com>
2024-01-18 21:45:37 +0530

    Update Signup.jsx

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -35,7 +35,7 @@ export const Signup = () => {
     e.preventDefault();
     // console.log(user);
 
-   const response  = await fetch('http://localhost:5000/api/auth/register', {
+   const response  = await fetch('https://notary-gadget.vercel.app/api/auth/register', {
     method:"POST",
     headers:{
       "Content-Type" : "application/json"
